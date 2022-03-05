bf03433005d8b30902c19d8544b78e831109fbc7
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-05 19:36:57 +0100

    Template Tags

--- index.php
+++ index.php
@@ -1,31 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>PHP for WordPress</title>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-</head>
-<body>
-
-  <header id="masthead">
-    <h1><a href="#">PHP for WordPress</a></h1>
-  </header>
+<?php get_header(); ?>
 
   <div id="content">
 
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<!-- Add any template tags outside of loop -->
 
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php endwhile; else : ?>
+      <!-- Add any post template tags inside of loop -->
 
-      <?php _e("Sorry! No content found!", "phpforwp"); ?>
+      <article <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <footer>
+          <p class="byline">
+            Author:
+            <a href="<?php echo get_author_posts_url($post->$post_author); ?>">
+              <?php the_author(); ?>
+            </a> |
+            Date:
+            <?php the_time('M. j, Y'); ?> |
+            Categories:
+            <?php the_category(','); ?> |
+            Tags:
+            <?php the_tags('', ', ', '') ?>
+          </p>
+        </footer>
+      </article>
+
+    <?php endwhile; else: ?>
+
+      <h2><?php esc_html_e( '404 Error', 'phpforwp' ); ?></h2>
+      <p><?php esc_html_e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
 
     <?php endif; ?>
 
+<!-- Add any template tags outside of loop -->
+
   </div>
 
-</body>
-</html>
+<?php get_footer(); ?>
