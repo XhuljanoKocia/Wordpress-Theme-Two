35be4de7fba7656a44a2a3a099585a44a48c1f95
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-14 00:22:26 +0100

    Adding Menus and body_class

--- header.php
+++ header.php
@@ -7,4 +7,13 @@
     <title>Solo Theme 2</title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+        <!-- We add our menu named main-menu to be displayed as our site menu in the header. -->
+        <?php 
+            $args = [
+                'theme_location' => 'main-menu'
+            ];
+            wp_nav_menu( $args ); 
+        ?>
+    </nav>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php get_header( 'splash' ); ?>
+<?php get_header(); ?>
     <h1>Solo Theme</h1>
-<?php get_footer( 'splash' ); ?>
+<?php get_footer(); ?>
