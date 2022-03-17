2e8c71cbc7d0734d01a85c554985490425dc0677
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-17 22:27:01 +0100

    Adding Markup to a Theme - Part 3

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
+    </div><!-- #content --> 
+        
+        <footer id="colophon" class="site-footer" role="contentinfo">
+
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'solothemetwo' ) ); ?>">
+                <?php printf( esc_html__( 'Proudly powered by %s', 'solothemetwo' ), 'Solo Coding' ); ?>
+            </a>
+       
+        </footer>
+    
+    </div><!-- #footer --> 
+
     <?php wp_footer(); ?>
 
-        </div><!-- #content --> 
-    </div><!-- #footer --> 
 </body>
 </html>
