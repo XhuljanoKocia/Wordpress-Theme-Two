c6eb5884e8e8db314aa17d752f66dbdc83c21f33
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-17 23:21:42 +0100

    Working with the Loop

--- index.php
+++ index.php
@@ -4,19 +4,43 @@
 
         <main id="main" class="site-main" role="main">
 
+        <?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
                 <header class="entry-header">
 
-                    <h1>Solo Theme</h1>
+                    <?php the_title( '<h1>', '</h1>' ); ?>
 
                 </header>
 
                 <div class="entry-content">
-                    <p>Some Text</p>
+
+                    <?php the_content(); ?>
+
                 </div>
 
             </article>
+
+        <?php endwhile; else : ?>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <header class="entry-header">
+
+                    <h1><?php esc_html_e( '404', 'solothemetwo' ); ?></h1>
+
+                </header>
+
+                <div class="entry-content">
+
+                    <p><?php esc_html_e( 'Sorry! Our programmer is in a dungeon right now. Check again later :D', 'solothemetwo' ); ?></p>
+
+                </div>
+
+            </article>
+
+        <?php endif; ?>
 
         </main>
 
